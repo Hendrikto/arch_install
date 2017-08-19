9a3d858829377d0d4d88c18247890e592b1d5427
Hendrikto <hendrik.to@gmail.com>
2017-08-19 03:41:36 +0200

    Add a yaourt adapter.

---
+++ backend/YaourtAdapter.py
@@ -0,0 +1,28 @@
+# author: Hendrik Werner <hendrik.to@gmail.com>
+
+import re
+import subprocess as sp
+from backend.Adapter import Adapter
+from typing import Iterable
+from typing import Optional
+
+
+class YaourtAdapter(Adapter):
+    @staticmethod
+    def get_package_info(name: str) -> Optional[tuple]:
+        try:
+            info = sp.run(
+                ["yaourt", "-Qi", name], stdout=sp.PIPE, check=True
+            ).stdout.decode()
+        except sp.CalledProcessError:
+            return
+        return (
+            name,
+            re.search("^Version\s*: (.*)$", info, re.MULTILINE)[1],
+            re.search("^Description\s*: (.*)$", info, re.MULTILINE)[1],
+            re.search("^Installed Size\s*: (.*)$", info, re.MULTILINE)[1],
+        )
+
+    @staticmethod
+    def install_packages(packages: Iterable) -> None:
+        sp.run("yaourt -S --noconfirm".split() + packages)
